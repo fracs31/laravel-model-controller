c02ef20b1dca2fc7dbfe01c4e6112ca8e0a86133
fracs31 <pist.fracs95@gmail.com>
2023-04-11 15:13:36 +0200

    FIX cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,12 +21,14 @@
                     {{-- Carta --}}
                     <div class="card" style="width: 18rem;">
                         <div class="card-body">
-                        <h5 class="card-title">{{ $movie["title"] }}</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie["original_title"] }}</h6>
-                        <a href="#" class="card-link">{{ $movie["nationality"] }}</a>
-                        <a href="#" class="card-link">{{ $movie["date"] }}</a>
-                        <a href="#" class="card-link">{{ $movie["vote"] }}</a>
+                          <h5 class="card-title">{{ $movie["title"] }}</h5>
+                          <p class="card-text">{{ $movie["original_title"] }}</p>
                         </div>
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item">{{ $movie["nationality"] }}</li>
+                          <li class="list-group-item">{{ $movie["date"] }}</li>
+                          <li class="list-group-item">{{ $movie["vote"] }}</li>
+                        </ul>
                     </div>
                 </div>
             @endforeach
