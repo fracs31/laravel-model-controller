192124338d74ee9ded67fe6f3a7f21ce3728e08b
fracs31 <pist.fracs95@gmail.com>
2023-04-11 14:58:12 +0200

    TRYING to get the informations from the database

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+    //Homepage
+    public function homepage() {
+        $movies = Movie::all(); //prendo i film
+        //Dati
+        $data = [
+            "movies" => $movies //film
+        ];
+        return view("welcome", $data); //restituisco la vista
+    }
 }
